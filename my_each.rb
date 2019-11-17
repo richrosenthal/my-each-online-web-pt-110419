339c0e07ca59d539618cf2eeb7ccd4ef1b9658e0
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-17 16:25:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,21 +1,21 @@
-def my_each 
-  
-end
-
-
-
-
-
-def hello_t(array)
-  i = 0
+def my_each(array)
+  if block_given?
+    i = 0
  
-  while i < array.length
-    i = i + 1
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
+
+my_each(array) do |word|
+  puts "#{word}"
 end
+
+
