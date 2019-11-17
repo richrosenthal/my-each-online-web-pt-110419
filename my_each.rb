24bbb11f18e0f932e87903e3bfc39005d30fd515
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-17 16:31:15 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    error_phrase = "Hey! No block was given!"
+    error_phrase
+  end
 end
+
+collection = [1,2,3,4]
+
+my_each(collection) do |word|
+   word
+end
+
+
